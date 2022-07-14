0a1b65e7bfb80545dee4cfde178e69097f54eb79
Riccardo Arca <riccardo@blowmedia.co.uk>
2022-07-14 08:26:22 +0100

    second commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+
+    const wSize = $(window).width();
+    
+    //enables Font Awesome pseudo elements
+    window.FontAwesomeConfig = {
+        searchPseudoElements: true
+    }
+
+    //add active class to body
+    $('body').addClass('active');
+
+});
